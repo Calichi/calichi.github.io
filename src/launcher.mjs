3b80e985d836ecb892393c60b0b11d7ed77ddbef
Carlos Hernández Alarcón <Calichi15@hotmail.com>
2024-07-03 21:39:26 -0500

    Cambiando header para movil

--- src/launcher.mjs
+++ src/launcher.mjs
@@ -13,14 +13,12 @@ buttonOpenMenu.addEventListener('click', (event) => {
     event.stopImmediatePropagation();
     buttonOpenMenu.style.display = 'none';
     buttonCloseMenu.style.display = 'inline-block';
-    header.style.backgroundColor = "var(--theme-15-percent)";
-    menuNavigation.style.display = 'inline';
+    menuNavigation.style.display = 'flex';
 });
 
 buttonCloseMenu.addEventListener('click', (event) => {
     event.stopImmediatePropagation();
     buttonCloseMenu.style.display = 'none';
     buttonOpenMenu.style.display = 'inline-block';
-    menu.style.backgroundColor = "#00000000";
     menuNavigation.style.display = 'none';
 });
